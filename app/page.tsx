177fbd809a2c2533424c373e683604ce3a227bf7
larsh0103 <larsvagnes@gmail.com>
2025-01-18 14:57:46 +0100

    bumb to mini model

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ interface avatarSettings {
 const avatar: avatarSettings = {
   name: "Frank",
   openai_voice: "echo",
-  openai_model: "gpt-4o-realtime-preview-2024-12-17", // Use "gpt-4o-mini-realtime-preview-2024-12-17" for cheaper and faster responses
-  simli_faceid: "5514e24d-6086-46a3-ace4-6a7264e5cb7c",
+  openai_model: "gpt-4o-mini-realtime-preview-2024-12-17", // Use "gpt-4o-mini-realtime-preview-2024-12-17" for cheaper and faster responses
+  simli_faceid: "6ebf0aa7-6fed-443d-a4c6-fd1e3080b215",
   initialPrompt:
     "You are a helpful AI assistant named Frank. You are friendly and concise in your responses. Your task is to help users with any questions they might have. Your answers are short and to the point, don't give long answers be brief and straightforward.",
 };
